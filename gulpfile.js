bf914dc41e543a61fa960e4c458a161fec775e55
JonatanPinheiro <jonatanpinheiro@outlook.com>
2017-09-27 18:31:21 -0300

    Site inteiramente reformulado

---
+++ gulpfile.js
@@ -0,0 +1,87 @@
+// Módulos do GULP
+var browserSync  = require('browser-sync').create();
+var gulp         = require('gulp');
+var autoprefixer = require('gulp-autoprefixer');
+var htmlMin      = require('gulp-htmlmin');
+var sass         = require('gulp-sass');
+var sourcemaps   = require('gulp-sourcemaps');
+var uglify       = require('gulp-uglify');
+var imagemin     = require('gulp-imagemin');
+
+
+
+// Variáveis de configuração
+var srcPath      = 'src/';
+var distPath     = 'dist/';
+var pluginPath     = 'plugin/';
+
+
+
+// Compressor de HTML
+gulp.task('html', function() {
+    gulp.src(srcPath + '*.html')
+        .pipe(htmlMin({collapseWhitespace: true}))
+        .pipe(gulp.dest(distPath))
+        .pipe(browserSync.reload({stream: true}));
+});
+
+
+// Processador de SASS, compressor, gerador de sourcemaps e autoprefixador de CSS
+gulp.task('sass', function(){
+    gulp.src(srcPath + 'sass/**/*.+(scss|sass)')
+        .pipe(sourcemaps.init())
+        .pipe(sass({outputStyle: 'compressed'}).on('error', sass.logError))
+        .pipe(autoprefixer())
+        .pipe(sourcemaps.write('./'))
+        .pipe(gulp.dest(distPath + 'css'))
+        .pipe(browserSync.reload({stream: true}));
+});
+
+
+// Compressor de Javascript
+gulp.task('js', function(){
+    gulp.src(srcPath + 'js/**/*.js')
+        .pipe(uglify())
+        .pipe(gulp.dest(distPath + 'js'))
+        .pipe(browserSync.reload({stream: true}));
+});
+
+
+// Compressor de imagens
+gulp.task('img', function(){
+    gulp.src(srcPath + 'img/**/*.+(jpg|png|gif)')
+//        .pipe(imagemin())
+        .pipe(gulp.dest(distPath + 'img'))
+        .pipe(browserSync.reload({stream: true}));
+});
+
+
+// Inicializador do BrowserSync
+gulp.task('browserSync', function() {
+    browserSync.init({
+        server: {
+            baseDir: './',
+        },
+        port: 8080,
+        startPath: '.',
+    });
+});
+
+
+// Copiador de plugins
+gulp.task('plugin', function(){
+    gulp.src(pluginPath + '**/*')
+        .pipe(gulp.dest(distPath))
+        .pipe(browserSync.reload({stream: true}));
+});
+
+
+
+// Tarefa padrão
+gulp.task('default', ['browserSync'], function() {
+    gulp.watch(srcPath + '**/*.html', ['html']);
+    gulp.watch(srcPath + 'sass/*.+(scss|sass)', ['sass']);
+    gulp.watch(srcPath + 'js/*.js', ['js']);
+    gulp.watch(srcPath + 'img/*.+(jpg|png|gif)', ['img']);
+    gulp.watch(pluginPath + '**/*', ['plugin']);
+});
