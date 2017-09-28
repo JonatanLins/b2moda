2f9cca1d4030a326337b51bc65e71d5a270f397a
JonatanPinheiro <jonatanpinheiro@outlook.com>
2017-09-27 22:27:12 -0300

    modificado top menu

--- init.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-npm init
-npm install --save-dev browser-sync gulp gulp-autoprefixer gulp-htmlmin gulp-imagemin gulp-sass gulp-sourcemaps gulp-uglify
-
-gulp html sass js img plugin
-gulp
-
-rm -f init.sh
