13b774e6418180f42d857898e34c137f0b3183ca
JonatanPinheiro <jonatanpinheiro@outlook.com>
2017-10-08 09:51:38 -0300

    color picker consertado

--- src/js/cadastro-produtos.js
+++ src/js/cadastro-produtos.js
@@ -35,14 +35,14 @@ $(document).ready(function () {
         var etapa = $('.etapas-cadastro .etapa.ativo');
 
         etapa.removeClass('ativo').prev('.etapa').addClass('ativo');
-        $('.guia-procedimento .etapa').removeClass('ativo');
+        $('.guia-procedimento .etapa.ativo').removeClass('ativo');
         $('.guia-procedimento .etapa:nth-child(' + etapa.prev('.etapa').data('passo') + ')').addClass('ativo');
     });
     $('.navegacao-etapas .proximo').click(function () {
         var etapa = $('.etapas-cadastro .etapa.ativo');
 
         etapa.removeClass('ativo').next('.etapa').addClass('ativo');
-        $('.guia-procedimento .etapa').removeClass('ativo');
+        $('.guia-procedimento .etapa.ativo').removeClass('ativo');
         $('.guia-procedimento .etapa:nth-child(' + etapa.next('.etapa').data('passo') + ')').addClass('ativo');
     });
 
@@ -63,6 +63,31 @@ $(document).ready(function () {
         } else {
             alert("Esta cor já existe!");
         }
+    });
+    
+    
+    
+    // Adiconar cor dinamicamente
+    $('#criar-cor .btn.salvar').click(function(){
+        var cor = $(this).closest('#criar-cor').find('.jscolor').css('background-color');
+        var nomeCor = $(this).closest('#criar-cor').find('.nome-cor').val().replace(/[^\w]/g, '');
+        
+        if (nomeCor == ''){
+            alert('Digite um nome!');
+        } else if (!$(this).closest('.etapa').find('.lista-cores #produto-cor-' + nomeCor).length){
+            $(this).closest('.etapa').find('.lista-cores').append(
+                '<fieldset>' +
+                    '<input type="radio" id="produto-cor-' + nomeCor + '" name="cor">' +
+                    '<label for="produto-cor-' + nomeCor + '">' +
+                        '<i class="fa fa-circle" aria-hidden="true" style="color:' + cor + '"></i> <span>' + nomeCor + '</span>' +
+                    '</label><br>' +
+                '</fieldset>'
+            );
+            $(this).closest('#criar-cor').find('.nome-cor').val('');
+        } else {
+            alert('Já existe uma cor com esse nome!');
+        }
+            
     });
 
 
