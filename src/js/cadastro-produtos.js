cc2672716c8adc0ad368082c798e67f8b4a1be61
JonatanPinheiro <jonatanpinheiro@outlook.com>
2017-10-11 20:16:30 -0300

    adicionado botao de excluir imagens do produto na página de cadastro de produto

--- src/js/cadastro-produtos.js
+++ src/js/cadastro-produtos.js
@@ -74,20 +74,28 @@ $(document).ready(function () {
             alert("Esta cor já existe!");
         }
     });
-    
-    
-    
+
+
+
     // Adicionar Preview das imagens do produto
-    $('#upload-imagens-produto').change(function(event){
-        while($('#preview-produto .owl-item').length){
+    function removerImagemPreview(){
+        $('#preview-produto').trigger('remove.owl.carousel', [$(this).closest('.owl-item').index()]).trigger('refresh.owl.carousel');
+    }
+    $('#upload-imagens-produto').change(function (event) {
+        while ($('#preview-produto .owl-item').length) {
             $('#preview-produto').trigger('remove.owl.carousel', [0]).trigger('refresh.owl.carousel');
         }
-        
-        for(i = 0; i < event.target.files.length; i++){
-            var imgTag = '<img class="item" src="' + URL.createObjectURL(event.target.files[i]) + '">';
+
+        for (i = 0; i < event.target.files.length; i++) {
+            var imgTag = '<div class="item"><img src="' + URL.createObjectURL(event.target.files[i]) +
+                '"><i class="fa fa-close" aria-hidden="true"></i></div>';
+
             $('#preview-produto').trigger('add.owl.carousel', [imgTag]).trigger('refresh.owl.carousel');
         }
+        
+        $('#preview-produto .item .fa-close').click(removerImagemPreview);
     });
+    $('#preview-produto .item .fa-close').click(removerImagemPreview);
 
 
 
@@ -144,12 +152,12 @@ $(document).ready(function () {
         $(this).closest('.cores-tamanhos-final li').addClass('hide');
         alert('Item excluído com sucesso!');
     });
-    
-    
-    
+
+
+
     // Trocar input ao selecionar opção de pagamento
-    $('.grupos.collapsible .seletor-modo-pagamento .dropdown-content li').click(function(){
-        if($(this).index() > 0){
+    $('.grupos.collapsible .seletor-modo-pagamento .dropdown-content li').click(function () {
+        if ($(this).index() > 0) {
             $(this).closest('tr').find('.preco-produto.ativo').removeClass('ativo');
             $(this).closest('tr').find('.preco-produto:nth-child(' + $(this).index() + ')').addClass('ativo');
         }
